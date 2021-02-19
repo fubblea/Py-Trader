d3475e54f86fa1448f605cca08e265bc79de49a0
fubblea <fubblea@gmail.com>
2021-02-19 12:03:13 +0400

    Move bot to seperate file

--- main.py
+++ main.py
@@ -1,32 +1,8 @@
 import alpaca_trade_api as alpaca
 import sys
-import so_trend
+import supertrend
 import datetime
 import print_supress
-
-class SuperTrend(object):
-    def __init__ (self, symbol):
-        self.key = 'PK1ZBENYCZ4YGCXIHT5P'
-        self.secret = 'WPM7Fr3RxYzZrUh4i7rkgxGn9xSuPkUXgFYwliOa'
-        self.alpaca_endpoint = 'https://paper-api.alpaca.markets'
-        self.api = alpaca.REST(self.key, self.secret, self.alpaca_endpoint)
-        self.symbol = symbol
-        self.current_order = None
-        
-        """
-        try:
-            self.position = int(self.api.get_position(self.symbol).qty)
-        except:
-            self.position = 0
-        """
-            
-    def submit_order(self, type, target):
-            
-        if type == "BUY":
-            self.current_order = self.api.submit_order(self.symbol, target, 'buy','market','fok')
-            
-        elif type == "SELL":
-            self.current_order = self.api.submit_order(self.symbol, target, 'sell','market','fok')
 
 if __name__ == '__main__':
     #TODO Implement last call through sessions
@@ -35,14 +11,14 @@ if __name__ == '__main__':
     symbol = "GME"
     last_call = "BUY"
     target = 50
-    t = SuperTrend(symbol)
+    t = supertrend.Bot(symbol)
     
     print(f"[{datetime.datetime.now()}]")
     print(f"Bot started with focus on {symbol}")
     while True:
         
         with print_supress.suppress_stdout_stderr():
-            current_call = so_trend.strat(symbol)
+            current_call = t.strat(symbol)
         
         if current_call == last_call:
             pass
