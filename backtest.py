d36723d49d411c896937780f9b979f119fb1c0f3
fubblea <ajay.arthur@gmail.com>
2021-02-20 13:06:36 +0400

    Cleaned up backtesting

--- backtest.py
+++ backtest.py
@@ -4,16 +4,9 @@ import supertrend
 
 parser = argparse.ArgumentParser()
 parser.add_argument("symbol", help="The symbol you want the bot to focus on")
-parser.add_argument("bias", help="Buying or selling bias")
 args = parser.parse_args()
 
 symbol = args.symbol
-    
-if args.bias == "buy":
-    last_call = "SELL"
-else:
-    last_call = "BUY"
-
 t = supertrend.Bot(symbol)
 
 data = t.analysis(symbol)
