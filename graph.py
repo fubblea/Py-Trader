43bce04b274de4f29cb6a085d7568071b4e9b8b2
fubblea <ajay.arthur@gmail.com>
2021-02-23 14:21:47 +0400

    Fixed ST

--- graph.py
+++ graph.py
@@ -1,10 +1,10 @@
 import mplfinance as mpf
-import datetime
 import pandas as pd
 
 def plot(df, symbol):
-    df.loc[:10,'ST'] = df.loc[10,'ST']
+    df.loc[0,'ST'] = df.loc[1,'ST']
     apdict = mpf.make_addplot(df['ST'], secondary_y=False)
     df = df.set_index('Datetime')
-    
-    mpf.plot(df,volume=True,addplot=apdict, type='candle', style='yahoo', title=symbol)
+        
+    mpf.plot(df,volume=False,addplot=apdict, type='candle', 
+            style='yahoo', title=symbol)
