a782587a2768a712dd670b020ea8004d4b4977dc
fubblea <ajay.arthur@gmail.com>
2021-02-23 14:25:24 +0400

    Cleaning

--- graph.py
+++ graph.py
@@ -1,21 +1,13 @@
 import mplfinance as mpf
 import pandas as pd
 
-<<<<<<< HEAD
+
 def plot(df, symbol):  
-=======
-def plot(df, symbol):
->>>>>>> mpf-graph
     df.loc[0,'ST'] = df.loc[1,'ST']
     apdict = mpf.make_addplot(df['ST'], secondary_y=False)
     df = df.set_index('Datetime')
-        
-<<<<<<< HEAD
     fig, ax = mpf.plot(df,volume=False,addplot=apdict, type='candle', 
                     style='yahoo', title=symbol, returnfig=True)
     
     return fig, ax
-=======
-    mpf.plot(df,volume=False,addplot=apdict, type='candle', 
-            style='yahoo', title=symbol)
->>>>>>> mpf-graph
+
