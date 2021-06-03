72a6ba3eb4b606f35ef57a004c04c2ce685d84ec
fubblea <ajay.arthur@gmail.com>
2021-06-03 13:12:37 +0400

    Fixed indexing error

--- backtest.py
+++ backtest.py
@@ -8,20 +8,24 @@ import os
 import alpaca_trade_api as alpaca
 
 
-class Supertrend(bt.Strategy):    
+class Supertrend(bt.Strategy):
+    
     def __init__(self):
         dotenv.load_dotenv()
         api = alpaca.REST(os.getenv("API_KEY"), os.getenv("SECRET_KEY"), os.getenv("ENDPOINT"))
         self.bot = supertrend.Bot(api=api, symbol='AAPL', backtest=True)
+        self.i = 0
     
     def next(self):
         print(self.datetime.date(ago=0))
-        print(self.bot.strat()[0])
+        print(self.bot.analysis().iloc[self.i]['ST_BUY_SELL'])
         
-        if self.bot.strat()[0] == 'BUY':
+        if self.bot.analysis().iloc[self.i]['ST_BUY_SELL'] == 'BUY':
             self.buy()
-        elif self.bot.strat()[0] == 'SELL':
+        elif self.bot.analysis().iloc[self.i]['ST_BUY_SELL'] == 'SELL':
             self.sell()
+        
+        self.i += 1
 
 symbol = 'AAPL'
 
@@ -29,7 +33,7 @@ cerebro = bt.Cerebro()
 cerebro.addstrategy(Supertrend)
 cerebro.broker.setcash(10000.0)
 
-dataframe = yf.download(symbol, start='2021-01-01', end='2021-01-30', interval='1d')
+dataframe = yf.download(symbol, start='2021-01-01', end='2021-03-30', interval='1d')
 dataframe.to_csv('backtesting_data.csv', encoding='utf-8')
 
 data = bt.feeds.PandasData(dataname=dataframe)    
