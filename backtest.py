68d8af60b7205ef980392232481c3ee173b57eaa
fubblea <ajay.arthur@gmail.com>
2021-06-03 17:25:23 +0400

    Partially implemented indicator

--- backtest.py
+++ backtest.py
@@ -1,4 +1,5 @@
 import backtrader as bt
+import backtrader.indicators as btind
 import math
 import supertrend
 import yfinance as yf
@@ -8,15 +9,40 @@ import os
 import alpaca_trade_api as alpaca
 
 
-class Supertrend(bt.Strategy):
-    params = (('order_pct', 1), ('ticker', 'AAPL'))
-    
+class DummyDifferenceDivider(bt.Indicator):
+    lines = ('diffdiv',)  # output line (array)
+
+    def __init__(self):
+        diff = self.data(0) - self.data(-3)
+        diffdiv = diff / 2
+        
+        print(diffdiv)
+        self.lines.diffdiv = diffdiv
+        
+class SupertrendLine(bt.Indicator):
+    lines = ('supertrend',)  # output line (array)
     def __init__(self):
         dotenv.load_dotenv()
         api = alpaca.REST(os.getenv("API_KEY"), os.getenv("SECRET_KEY"), os.getenv("ENDPOINT"))
-        self.bot = supertrend.Bot(api=api, symbol=self.params.ticker, backtest=True)
+        self.bot = supertrend.Bot(api=api, symbol=symbol, backtest=True)
+        
+        self.st_data = self.bot.analysis()['ST'].tolist() + self.data
+        self.st_data = self.st_data - self.data
+        
+        self.lines.supertrend = self.st_data
+
+class Supertrend(bt.Strategy):
+    
+    def __init__(self):
+        #DummyDifferenceDivider(self.data)
+        #SupertrendLine(self.data)
+        
+        dotenv.load_dotenv()
+        api = alpaca.REST(os.getenv("API_KEY"), os.getenv("SECRET_KEY"), os.getenv("ENDPOINT"))
+        self.bot = supertrend.Bot(api=api, symbol=symbol, backtest=True)
         self.i = 0
         self.last_call = 'HOLD'
+        self.order_pct = 1
     
     def next(self):
         print(self.datetime.date(ago=0))
@@ -27,18 +53,18 @@ class Supertrend(bt.Strategy):
                 print("HOLD")
                 
             elif self.bot.strat(self.i)[0] == "BUY":
-                amount_to_invest = (self.p.order_pct * self.broker.cash)
+                amount_to_invest = (self.order_pct * self.broker.cash)
                 self.size = math.floor(amount_to_invest / self.data.close)
 
-                print("Buy {} shares of {} at {}".format(self.size, self.params.ticker, self.data.close[0]))
+                print("Buy {} shares of {} at {}".format(self.size, symbol, self.data.close[0]))
                 self.buy(size=self.size)
                 print("BUY")
                 
             else:
-                amount_to_invest = (self.p.order_pct * self.broker.cash)
+                amount_to_invest = (self.order_pct * self.broker.cash)
                 self.size = math.floor(amount_to_invest / self.data.close)
 
-                print("Sell {} shares of {} at {}".format(self.size, self.params.ticker, self.data.close[0]))
+                print("Sell {} shares of {} at {}".format(self.size, symbol, self.data.close[0]))
                 self.sell(size=self.size)
                 print("SELL")
         else:
@@ -55,6 +81,7 @@ class Supertrend(bt.Strategy):
         self.last_call = self.bot.strat(self.i)[0]
         self.i += 1
 
+global symbol
 symbol = 'AAPL'
 
 cerebro = bt.Cerebro()
