df31fe323686501f1d5d2cabfe904b750566af76
fubblea <ajay.arthur@gmail.com>
2021-06-03 13:38:03 +0400

    Added order percentage calcualtions

--- backtest.py
+++ backtest.py
@@ -1,5 +1,5 @@
 import backtrader as bt
-import datetime
+import math
 import supertrend
 import yfinance as yf
 
@@ -9,22 +9,50 @@ import alpaca_trade_api as alpaca
 
 
 class Supertrend(bt.Strategy):
+    params = (('order_pct', 1), ('ticker', 'AAPL'))
     
     def __init__(self):
         dotenv.load_dotenv()
         api = alpaca.REST(os.getenv("API_KEY"), os.getenv("SECRET_KEY"), os.getenv("ENDPOINT"))
-        self.bot = supertrend.Bot(api=api, symbol='AAPL', backtest=True)
+        self.bot = supertrend.Bot(api=api, symbol=self.params.ticker, backtest=True)
         self.i = 0
+        self.last_call = 'HOLD'
     
     def next(self):
         print(self.datetime.date(ago=0))
-        print(self.bot.strat(self.i)[0])
         
-        if self.bot.strat(self.i)[0] == 'BUY':
-            self.buy()
-        elif self.bot.strat(self.i)[0] == 'SELL':
-            self.sell()
+        if self.position.size == 0:                
+            if self.bot.strat(self.i)[0] == 'HOLD':
+                pass
+                print("HOLD")
+                
+            elif self.bot.strat(self.i)[0] == "BUY":
+                amount_to_invest = (self.p.order_pct * self.broker.cash)
+                self.size = math.floor(amount_to_invest / self.data.close)
+
+                print("Buy {} shares of {} at {}".format(self.size, self.params.ticker, self.data.close[0]))
+                self.buy(size=self.size)
+                print("BUY")
+                
+            else:
+                amount_to_invest = (self.p.order_pct * self.broker.cash)
+                self.size = math.floor(amount_to_invest / self.data.close)
+
+                print("Sell {} shares of {} at {}".format(self.size, self.params.ticker, self.data.close[0]))
+                self.sell(size=self.size)
+                print("SELL")
+        else:
+            if self.last_call == 'BUY' and self.bot.strat(self.i)[0] == 'SELL':
+                self.close()
+                print("Close Positions")
+                
+            elif self.last_call == 'SELL' and self.bot.strat(self.i)[0] == 'BUY':
+                self.close()
+                print("Close Positions")
+            else:
+                print("HOLD")
         
+        self.last_call = self.bot.strat(self.i)[0]
         self.i += 1
 
 symbol = 'AAPL'
@@ -33,7 +61,7 @@ cerebro = bt.Cerebro()
 cerebro.addstrategy(Supertrend)
 cerebro.broker.setcash(10000.0)
 
-dataframe = yf.download(symbol, start='2021-01-01', end='2021-03-30', interval='1d')
+dataframe = yf.download(symbol, start='2021-01-01', end='2021-04-05', interval='1d')
 dataframe.to_csv('backtesting_data.csv', encoding='utf-8')
 
 data = bt.feeds.PandasData(dataname=dataframe)    
